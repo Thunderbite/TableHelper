dccd9e170acb76117bf119be6929054c65c6f9e2
Radu Tirnovan <radutirnovan@Radus-MacBook-Pro.local>
2018-05-08 15:42:09 +0300

    cleanup ButtonHelper

--- src/ButtonHelper.php
+++ src/ButtonHelper.php
@@ -30,13 +30,6 @@ class ButtonHelper
         return '<a href="' . $href . '" class="btn btn-default btn-xs"><i class="fas fa-pencil-alt"></i></a>';
     }
 
-    // public static function delete($model, $id)
-    // {
-    //
-
-    //     return '<a href="' . $href . '" class="btn btn-default deleteButton btn-xs" data-method=""><i class=""></i></a>';
-    // }
-
     public static function groupEditDelete($model, $id)
     {
         $return = '<div class="btn-group btn-group-xs">';
@@ -51,7 +44,7 @@ class ButtonHelper
     {
         $return = '<div class="btn-group btn-group-xs">';
 
-        $return .= '<a href="/backstage/game-periods/' . $id . '/use/" class="btn btn-default editButton"><i class="fas fa-play fa-fw"></i></a>';
+        $return .= '<a href="/backstage/' . $model . '/' . $id . '/use/" class="btn btn-default editButton"><i class="fas fa-play fa-fw"></i></a>';
         
         if (!auth()->user()->readonly) {
             $return .= '<a href="' . URL::route($model . '.edit', $id) . '" class="btn btn-default editButton"><i class="fas fa-pencil-alt fa-fw"></i></a>';
@@ -63,24 +56,24 @@ class ButtonHelper
         return $return;
     }
 
-    // public static function groupSelectLockEditDelete($model, $id)
-    // {
-    //     // $destoryHref = "/admin/$model/$id";
-    //     // $lockHref = "/admin/$model/$id/lock";
-    //     $editHref = "/admin/$model/$id/edit";
-    //     $selectHref = "/admin/$model/use/$id";
-    //     // $cloneHref = "/admin/$model/$id/clone";
+    public static function groupSelectLockEditDelete($model, $id)
+    {
+        // $destoryHref = "/admin/$model/$id";
+        // $lockHref = "/admin/$model/$id/lock";
+        $editHref = "/admin/$model/$id/edit";
+        $selectHref = "/admin/$model/use/$id";
+        // $cloneHref = "/admin/$model/$id/clone";
 
-    //     $return = '<div class="btn-group btn-group-sm">';
+        $return = '<div class="btn-group btn-group-sm">';
 
-    //     $return .= '<a href="' . $selectHref . '" class="btn btn-default editButton"><i class="fa fa-play"></i></a>';
-    //     // $return .= '<a href="'.$lockHref.'" class="btn btn-default editButton"><i class="fa fa-unlock-alt"></i></a>';
-    //     $return .= '<a href="' . $editHref . '" class="btn btn-default editButton"><i class="fas fa-pencil-alt"></i></a>';
-    //     // $return .= '<a href="'.$cloneHref.'" class="btn btn-default editButton"><i class="fa fa-clone"></i></a>';
-    //     // $return .= '<a href="'.$destoryHref.'" class="btn btn-default deleteButton" data-method="DELETE"><i class="fas fa-trash-alt"></i></a>';
+        $return .= '<a href="' . $selectHref . '" class="btn btn-default editButton"><i class="fa fa-play"></i></a>';
+        // $return .= '<a href="'.$lockHref.'" class="btn btn-default editButton"><i class="fa fa-unlock-alt"></i></a>';
+        $return .= '<a href="' . $editHref . '" class="btn btn-default editButton"><i class="fas fa-pencil-alt"></i></a>';
+        // $return .= '<a href="'.$cloneHref.'" class="btn btn-default editButton"><i class="fa fa-clone"></i></a>';
+        // $return .= '<a href="'.$destoryHref.'" class="btn btn-default deleteButton" data-method="DELETE"><i class="fas fa-trash-alt"></i></a>';
 
-    //     $return .= '</div>';
+        $return .= '</div>';
 
-    //     return $return;
-    // }
+        return $return;
+    }
 }
