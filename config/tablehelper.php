4853bb8a526fbecc76292e45a4f9c0cd1d285075
Mark van der Putten <mark@codeva.io>
2018-05-18 12:28:02 +0300

    Typo fix

--- config/tablehelper.php
+++ config/tablehelper.php
@@ -6,6 +6,6 @@ return [
     /*
      * Define used bootstrap version that also relates to fontawesome syntax
      */
-    'boostrap_version' => env('THUNDERBITE_BOOTSTRAP_VERSION', 4),
+    'bootstrap_version' => env('THUNDERBITE_BOOTSTRAP_VERSION', 4),
     
 ];
