ba99173c19ca3d50b914a3ffca3e6b6035da8a96
Mark van der Putten <mark@codeva.io>
2018-05-18 13:57:34 +0300

    Reverting to named routes

--- src/ButtonHelper.php
+++ src/ButtonHelper.php
@@ -23,7 +23,8 @@ class ButtonHelper
         // Setup
         self::setup();
 
-        return '<a href="/admin/' . $model . '/log/' . $id . '" class="btn btn-default btn-' . self::$bootstrapButtonSize . '"><i class="' . self::$fontawesomeClass . ' fa-eye"></i></a>';
+        // Return html
+        return '<a href="' . URL::route($model . '.log', $id) . '" class="btn btn-default btn-' . self::$bootstrapButtonSize . '"><i class="' . self::$fontawesomeClass . ' fa-eye"></i></a>';
     }
 
     public static function delete($model, $id)
@@ -31,7 +32,8 @@ class ButtonHelper
         // Setup
         self::setup();
 
-        return '<a href="' . URL::route($model . '.destroy', $id) . '" class="btn btn-default btn-' . self::$bootstrapButtonSize . '" data-method="DELETE"><i class="' . self::$fontawesomeClass . ' fa-trash-alt"></i></a>';
+        // Return html
+        return '<a href="' . URL::route($model . '.destroy', $id) . '" class="btn btn-default btn-' . self::$bootstrapButtonSize . '" data-method="DELETE"><i class="' . self::$fontawesomeClass . ' fa-trash"></i></a>';
     }
 
     public static function edit($model, $id)
@@ -39,7 +41,8 @@ class ButtonHelper
         // Setup
         self::setup();
 
-        return '<a href="' . URL::route($model . '.edit', $id) . '" class="btn btn-default btn-' . self::$bootstrapButtonSize . '"><i class="' . self::$fontawesomeClass . ' fa-pencil-alt"></i></a>';
+        // Return html
+        return '<a href="' . URL::route($model . '.edit', $id) . '" class="btn btn-default btn-' . self::$bootstrapButtonSize . '"><i class="' . self::$fontawesomeClass . ' fa-pencil"></i></a>';
     }
 
     public static function groupEditDelete($model, $id)
@@ -47,9 +50,10 @@ class ButtonHelper
         // Setup
         self::setup();
 
+        // Return html
         $return = '<div class="btn-group btn-group-' . self::$bootstrapButtonSize . '">';
-        $return .= '<a href="' . URL::route($model . '.edit', $id) . '" class="btn btn-default"><i class="' . self::$fontawesomeClass . ' fa-pencil-alt"></i></a>';
-        $return .= '<a href="' . URL::route($model . '.destroy', $id) . '" class="btn btn-default" data-method="DELETE"><i class="' . self::$fontawesomeClass . ' fa-trash-alt"></i></a>';
+        $return .= '<a href="' . URL::route($model . '.edit', $id) . '" class="btn btn-default"><i class="' . self::$fontawesomeClass . ' fa-pencil"></i></a>';
+        $return .= '<a href="' . URL::route($model . '.destroy', $id) . '" class="btn btn-default" data-method="DELETE"><i class="' . self::$fontawesomeClass . ' fa-trash"></i></a>';
         $return .= '</div>';
 
         return $return;
@@ -60,31 +64,13 @@ class ButtonHelper
         // Setup
         self::setup();
 
-
-        $destoryHref = URL::route($model . '.destroy', $id);
-        $editHref = URL::route($model . '.edit', $id);
-        $selectHref = '/admin/campaigns/use/' . $id;
-
-        if (config('tablehelper.bootstrap_version') == 3) {
-            $return = '<div class="btn-group btn-group-sm">';
-
-            $return .= '<a href="' . $selectHref . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-play"></i></a>';
-
-            if (!auth()->user()->readonly) {
-                $return .= '<a href="' . $editHref . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-pencil"></i></a>';
-                $return .= '<a href="' . $destoryHref . '" class="btn btn-default deleteButton" data-method="DELETE"><i class="' . self::$fontawesomeClass . ' fa-trash-o"></i></a>';
-            }
-            $return .= '</div>';
-
-            return $return;
-        }
+        // Return html
         $return = '<div class="btn-group btn-group-' . self::$bootstrapButtonSize . '">';
-
-        $return .= '<a href="' . $selectHref . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-play fa-fw"></i></a>';
+        $return .= '<a href="' . URL::route($model . '.use', $id) . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-play fa-fw"></i></a>';
 
         if (!auth()->user()->readonly) {
-            $return .= '<a href="' . $editHref . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-pencil-alt fa-fw"></i></a>';
-            $return .= '<a href="' . $destoryHref . '" class="btn btn-default deleteButton" data-method="DELETE"><i class="' . self::$fontawesomeClass . ' fa-trash-alt fa-fw"></i></a>';
+            $return .= '<a href="' . URL::route($model . '.edit', $id) . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-pencil fa-fw"></i></a>';
+            $return .= '<a href="' . URL::route($model . '.destroy', $id) . '" class="btn btn-default deleteButton" data-method="DELETE"><i class="' . self::$fontawesomeClass . ' fa-trash fa-fw"></i></a>';
         }
 
         $return .= '</div>';
@@ -98,18 +84,12 @@ class ButtonHelper
         self::setup();
 
         // Setup routes
-        $destoryHref = "/admin/$model/$id";
-        $lockHref = "/admin/$model/$id/lock";
-        $editHref = "/admin/$model/$id/edit";
-        $selectHref = "/admin/$model/use/$id";
-        $cloneHref = "/admin/$model/$id/clone";
-
         $return = '<div class="btn-group btn-group-' . self::$bootstrapButtonSize . '">';
-        $return .= '<a href="' . $selectHref . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-play"></i></a>';
-        $return .= '<a href="' . $lockHref . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-unlock-alt"></i></a>';
-        $return .= '<a href="' . $editHref . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-pencil-alt"></i></a>';
-        $return .= '<a href="' . $cloneHref . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-clone"></i></a>';
-        $return .= '<a href="' . $destoryHref . '" class="btn btn-default deleteButton" data-method="DELETE"><i class="' . self::$fontawesomeClass . ' fa-trash-alt"></i></a>';
+        $return .= '<a href="' . URL::route($model . '.use', $id) . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-play"></i></a>';
+        $return .= '<a href="' . URL::route($model . '.lock', $id) . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-unlock-alt"></i></a>';
+        $return .= '<a href="' . URL::route($model . '.edit', $id) . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-pencil"></i></a>';
+        $return .= '<a href="' . URL::route($model . '.clone', $id) . '" class="btn btn-default editButton"><i class="' . self::$fontawesomeClass . ' fa-clone"></i></a>';
+        $return .= '<a href="' . URL::route($model . '.destroy', $id) . '" class="btn btn-default deleteButton" data-method="DELETE"><i class="' . self::$fontawesomeClass . ' fa-trash"></i></a>';
         $return .= '</div>';
 
         return $return;
@@ -138,7 +118,7 @@ class ButtonHelper
     }
 
     public static function groupViewEdit($model, $id)
-    {   
+    {
         // Setup routes
         $return = '<div class="btn-group btn-group-' . self::$bootstrapButtonSize . '">';
 
