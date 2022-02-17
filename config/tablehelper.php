c2583cb1306962ad96d610616b65d66200203eca
Rohan Sakhale <rohansakhale@gmail.com>
2022-02-17 22:17:56 +0530

    Table Helper class configurable

--- config/tablehelper.php
+++ config/tablehelper.php
@@ -1,5 +1,6 @@
 <?php
 
+use Thunderbite\TableHelper\ButtonHelper;
 
 return [
 
@@ -7,5 +8,7 @@ return [
      * Define used bootstrap version that also relates to fontawesome syntax
      */
     'bootstrap_version' => env('THUNDERBITE_BOOTSTRAP_VERSION', 4),
-    
+
+    'button_helper' => ButtonHelper::class,
+
 ];
